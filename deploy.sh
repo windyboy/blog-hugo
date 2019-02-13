24658fdd68cc2bcddd453e429624307606d90496
windyboy <windyboy@gmail.com>
2019-02-13 14:27:29 +0800

    update deploty

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace by `hugo -t <yourtheme>`
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
@@ -21,5 +20,5 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
-# Come Back
+# Come Back up to the Project Root
 cd ..
